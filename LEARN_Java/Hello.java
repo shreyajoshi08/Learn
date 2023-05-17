02be6d4726b3a3db997d6572fd293191a835384c
shreyajoshi08 <49077392+shreyajoshi08@users.noreply.github.com>
2023-05-17 13:32:29 -0400

    Add files via upload

---
+++ LEARN_Java/Hello.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        int myFirstNum = 5;
+        System.out.println(myFirstNum);
+        
+        int mySecondNum = 12;
+        System.out.println(mySecondNum);
+        
+        int myThirdNum = 6;
+        System.out.println(myThirdNum);
+        
+        int myTotal = myFirstNum+mySecondNum+myThirdNum;
+        System.out.println(myTotal);
+    }
+}
