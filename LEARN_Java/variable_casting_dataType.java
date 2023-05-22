e3d03d7fc6bee61457bb4efec18e2f472035617c
shreyajoshi08 <49077392+shreyajoshi08@users.noreply.github.com>
2023-05-22 11:50:21 -0400

    Update variable_casting_dataType.java

--- LEARN_Java/variable_casting_dataType.java
+++ LEARN_Java/variable_casting_dataType.java
@@ -38,3 +38,30 @@ public class Hello {
         
     }
 } 
+
+
+/*
+ADDITIONAL CASTING EXAMPLES
+public class Main {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);      // Outputs 9
+    System.out.println(myDouble);   // Outputs 9.0
+  }
+}
+
+
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
+
+
+*/
