8c2f6e1fdcf4aa129778dcacf314bcc6994ca3fc
shreyajoshi08 <49077392+shreyajoshi08@users.noreply.github.com>
2024-04-24 19:39:27 -0400

    Delete Pre_processing.py

--- Pre_processing.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-dataset = pd.read_csv("/Users/shreyajoshi/Documents/LEARN/LEARN_ML/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/Data.csv")
-dataset
