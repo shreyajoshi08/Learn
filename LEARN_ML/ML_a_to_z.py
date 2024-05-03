2a918793a2d6224ff2af166b0e06990d4f7c063f
shreyajoshi08 <49077392+shreyajoshi08@users.noreply.github.com>
2024-05-03 10:27:47 -0400

    Update ML_a_to_z.py

--- LEARN_ML/ML_a_to_z.py
+++ LEARN_ML/ML_a_to_z.py
@@ -53,3 +53,26 @@ from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder() #no is 0 and 1 is yes
 y= le.fit_transform(y)
 y
+#dataset.drop("Salary", axis=1)
+#dataset.drop(2) #drops 3rd row, Python Indexes from 0
+df = pd.DataFrame(dataset)
+encoded_df = pd.get_dummies(df, columns={"Country"})
+encoded_df
+print(x)
+print(y)
+
+#Train Test Split
+from sklearn.model_selection import train_test_split
+x_train,x_test, y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=1)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:,3:] = sc.fit_transform(x_train[:,3:])
+x_test[:,3:] = sc.transform(x_test[:,3:])
+print(x_train)
+print(x_test)
